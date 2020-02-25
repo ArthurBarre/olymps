f0c3483df0e1a9a1002dffe89d83b539aaeb4591
11Geoffreyg <geoffreyjguez@gmail.com>
2020-02-25 11:59:53 +0100

    first style integration

---
+++ front/src/components/Practice/Practice.js
@@ -0,0 +1,81 @@
+import react from 'react';
+
+function Practice () {
+        return(
+            <div>
+                <ul>
+                    <li>
+                        Athletics
+                    </li>
+                    <li>
+                        Armchair basket
+                    </li>
+                    <li>
+                        Boccia
+                    </li>
+                    <li>
+                        Blind Football
+                    </li>
+                    <li>
+                        Goalball
+                    </li>
+                    <li>
+                        Para athletics
+                    </li>
+                    <li>
+                        Para rowing
+                    </li>
+                    <li>
+                        Para badminton
+                    </li>
+                    <li>
+                        Para canoe
+                    </li>
+                    <li>
+                        Para cycling (road and track)
+                    </li>
+                    <li>
+                        Para horse riding (dressage)
+                    </li>
+                    <li>
+                        Para fencing
+                    </li>
+                    <li>
+                        Para weightlifting
+                    </li>
+                    <li>
+                        Para judo
+                    </li>
+                    <li>
+                        Para swimming
+                    </li>
+                    <li>
+                        Para taekwondo
+                    </li>
+                    <li>
+                        Para table tennis
+                    </li>
+                    <li>
+                        Para archery
+                    </li>
+                    <li>
+                        Para shooting sport
+                    </li>
+                    <li>
+                        Para triathlon
+                    </li>
+                    <li>
+                        Armchair rugby
+                    </li>
+                    <li>
+                        Chair tennis
+                    </li>
+                    <li>
+                        Sitting volleyball
+                    </li>
+                </ul>
+            </div>
+        )
+    
+}
+export default Practice;
