e235ab7a068275a0dbaf9b6a4a41ae8aa1f6d06d
hlnmargary <margary.helene@hetic.net>
2020-02-26 15:22:53 +0100

    infosMap component

---
+++ front/src/components/ThreeJs/MapInfos.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class MapInfos extends React.Component {
+
+  render() {
+    return (
+      <div class="infos-container">
+        <div class="aera-name"></div>
+        <div class="aera-infos">
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+};
+
+
+export default MapInfos;
--- front/src/components/ThreeJs/Three.js
+++ front/src/components/ThreeJs/Three.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import Canvas from './Canvas'
-import MapVideo from './MapVideo'
+import React from 'react';
+import Canvas from './Canvas';
+import MapInfos from './MapInfos';
 
 class Three extends React.Component {
   render() {
     return (
       <div className="threeContainer">
+        <div className="infos-place">
+          <MapInfos />
+        </div>
         <div className="canvas">
           <Canvas svg={this.props.svg} />
         </div>
-        {/* <div className="threeVideo">
-          <MapVideo />
-        </div> */}
       </div>
     )
   }
