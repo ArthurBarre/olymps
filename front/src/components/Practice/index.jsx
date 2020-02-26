890a8e1799b47dfe61cf7cf9de7a4cbb1856a609
11Geoffreyg <geoffreyjguez@gmail.com>
2020-02-26 09:34:20 +0100

    more modifications paris 2024 page

--- front/src/components/Practice/index.jsx
+++ front/src/components/Practice/index.jsx
@@ -5,33 +5,31 @@ export default function Practice () {
   return(
     <div className="general">
       <ul className="sports_list">
-          <li>Armchair basket</li>
-          <li>Boccia</li>
-          <li>Blind Football</li>
-          <li>Goalball</li>
-          <li>Para athletics</li>
-          <li>Para rowing</li>
-          <li>Para badminton</li>
-          <li>Para canoe</li>
-          <li>Para cycling</li>
-          <li>Para horse riding</li>
-          <li>Para fencing</li>
-          <li>Para weightlifting</li>
-          <li>Para judo</li>
-          <li>Para swimming</li>
-          <li>Para taekwondo</li>
-          <li>Para table tennis</li>
-          <li>Para archery</li>
-          <li>Para shooting sport</li>
-          <li>Para triathlon</li>
-          <li>Armchair rugby</li>
-          <li>Chair tennis</li>
-          <li>Sitting volleyball</li>
+          <li className="sports_list_li">Armchair basket</li>
+          <li className="sports_list_li">Boccia</li>
+          <li className="sports_list_li">Blind Football</li>
+          <li className="sports_list_li">Goalball</li>
+          <li className="sports_list_li">Para athletics</li>
+          <li className="sports_list_li">Para rowing</li>
+          <li className="sports_list_li">Para badminton</li>
+          <li className="sports_list_li">Para canoe</li>
+          <li className="sports_list_li">Para cycling</li>
+          <li className="sports_list_li">Para horse riding</li>
+          <li className="sports_list_li">Para fencing</li>
+          <li className="sports_list_li">Para weightlifting</li>
+          <li className="sports_list_li">Para judo</li>
+          <li className="sports_list_li">Para swimming</li>
+          <li className="sports_list_li">Para taekwondo</li>
+          <li className="sports_list_li">Para table tennis</li>
+          <li className="sports_list_li">Para archery</li>
+          <li className="sports_list_li">Para shooting sport</li>
+          <li className="sports_list_li">Para triathlon</li>
+          <li className="sports_list_li">Armchair rugby</li>
+          <li className="sports_list_li">Chair tennis</li>
+          <li className="sports_list_li">Sitting volleyball</li>
       </ul>
       <h1 className="title">Where can you practice in paris ?</h1>
-      <button>
-        <img href=" /assets/img/arrow-bottom.png"></img>
-      </button>
+      <button className="map_button"></button>
     </div>
   )
 }
